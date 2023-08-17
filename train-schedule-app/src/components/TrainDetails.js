e6251e546267cb4930bb947427fdc06ee00ef501
srinivas <piserla.srinivas@gmail.com>
2023-08-17 13:43:21 +0530

    updated

--- train-schedule-app/src/components/TrainDetails.js
+++ train-schedule-app/src/components/TrainDetails.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
-const TrainDetails = ({ train }) => {
+const TrainDetails = () => {
   return (
-    <div>
-      {/* Display train details */}
+    <div className="bg-white rounded-lg shadow-lg p-6">
+      <h2 className="text-2xl font-semibold mb-3">Train Details</h2>
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <p className="text-gray-700 text-sm font-bold">Train Name:</p>
+          <p className="text-gray-800">Train ABC</p>
+        </div>
+        <div>
+          <p className="text-gray-700 text-sm font-bold">Train Number:</p>
+          <p className="text-gray-800">12345</p>
+        </div>
+        <div>
+          <p className="text-gray-700 text-sm font-bold">Train Type:</p>
+          <p className="text-gray-800">Express</p>
+        </div>
+        <div>
+          <p className="text-gray-700 text-sm font-bold">Train Route:</p>
+          <p className="text-gray-800">Route 1</p>
+        
+       
+      </div>
+      <p className="mt-4 text-sm text-gray-600">
+        Explore the details of this train for a comfortable journey.
+      </p>
+      <div className="mt-6">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          Book Ticket
+        </button>
+      </div>
+      <p className="mt-4 text-sm text-gray-600">
+        Note: Booking availability and fare may vary.
+      </p>
     </div>
   );
 };
--- train-schedule-app/src/components/TrainList.js
+++ train-schedule-app/src/components/TrainList.js
@@ -2,13 +2,39 @@ import React from 'react';
 
 const TrainList = () => {
   return (
-    <div>
-      <h2>Train List</h2>
-      <h4>train discription</h4>
-      <p>Train 1</p>
-      <p>Train 2</p>
-      <p>Train 3</p>
-
+    <div className="bg-white rounded-lg shadow-lg p-6">
+      <h2 className="text-2xl font-semibold mb-3">Available Trains</h2>
+      <p className="text-gray-700 mb-4">
+        Browse through the list of trains to find the perfect one for your journey.
+      </p>
+      <ul className="list-disc list-inside space-y-2">
+        <li className="text-gray-800">Train 1</li>
+        <li className="text-gray-800">Train 2</li>
+        <li className="text-gray-800">Train 3</li>
+        {/* Add more trains here */}
+      </ul>
+      <p className="mt-4 text-sm text-gray-600">
+        Note: Train availability may vary based on schedules and routes.
+      </p>
+      <div className="mt-6">
+        <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+          View Train Details
+        </button>
+      </div>
+      <p className="mt-4 text-sm text-gray-600">
+        Note: You will be redirected to the train details page.
+      </p>
+      <hr className="mt-8" />
+      <div className="px-4 py-5 sm:p-6">
+        <p className="text-sm leading-5 font-medium text-indigo-600">
+          <a
+            href="/"
+            className="text-indigo-600 hover:text-indigo-500 transition ease-in-out duration-150"
+          >
+            Back to All Trains
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
